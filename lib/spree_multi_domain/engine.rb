d82359c3c4d2ac10fe265913dcbb3e9ec2671449
Nicolas Roumeliotis <info@nickthegeek.gr>
2018-04-25 15:17:13 +0300

    deprecated syntax fixes

--- lib/spree_multi_domain/engine.rb
+++ lib/spree_multi_domain/engine.rb
@@ -11,51 +11,61 @@ module SpreeMultiDomain
         end
       end
 
-      Spree::Config.searcher_class = Spree::Search::MultiDomain
+      Spree::Config.searcher_class = Object.const_defined?('Spree::Search::MySearchkick') ? Spree::Search::MySearchkick : Spree::Search::MultiDomain
       ApplicationController.send :include, SpreeMultiDomain::MultiDomainHelpers
     end
 
     config.to_prepare &method(:activate).to_proc
 
     initializer "templates with dynamic layouts" do |app|
-      ActionView::TemplateRenderer.class_eval do
-        def find_layout_with_multi_store(layout, locals, *formats)
-          store_layout = layout
+      ActionView::TemplateRenderer.prepend(
+        Module.new do
+          def find_layout(layout, locals, formats=[])
+            store_layout = layout
+            if @view.respond_to?(:current_store) && @view.current_store && !@view.controller.is_a?(Spree::Admin::BaseController) && !@view.controller.is_a?(Spree::Api::BaseController)
+              store_layout = if layout.is_a?(String)
+                layout.gsub("layouts/", "layouts/#{@view.current_store.code}/")
+              else
+                layout.call.try(:gsub, "layouts/", "layouts/#{@view.current_store.code}/")
+              end
+            end
 
-          if @view.respond_to?(:current_store) && @view.current_store && !@view.controller.is_a?(Spree::Admin::BaseController)
-            store_layout = if layout.is_a?(String)
-              layout.gsub("layouts/", "layouts/#{@view.current_store.code}/")
-            else
-              layout.call.try(:gsub, "layouts/", "layouts/#{@view.current_store.code}/")
+            begin
+
+              if Rails.gem_version >= Gem::Version.new('5.x') # hack to make it work with rails 4.x and 5.x
+                super(store_layout, locals, formats)
+              else
+                super(store_layout, locals, *formats)
+              end
+
+            rescue ::ActionView::MissingTemplate
+              if Rails.gem_version >= Gem::Version.new('5.x') # hack to make it work with rails 4.x and 5.x
+                super(layout, locals, formats)
+              else
+                super(layout, locals, *formats)
+              end
             end
           end
-
-          begin
-            find_layout_without_multi_store(store_layout, locals, *formats)
-          rescue ::ActionView::MissingTemplate
-            find_layout_without_multi_store(layout, locals, *formats)
-          end
         end
-
-        alias_method_chain :find_layout, :multi_store
-      end
+      )
     end
 
     initializer "current order decoration" do |app|
       require 'spree/core/controller_helpers/order'
-      ::Spree::Core::ControllerHelpers::Order.module_eval do
-        def current_order_with_multi_domain(options = {})
-          options[:create_order_if_necessary] ||= false
-          current_order_without_multi_domain(options)
+      ::Spree::Core::ControllerHelpers::Order.prepend(
+        Module.new do
+          def current_order(options = {})
+            options[:create_order_if_necessary] ||= false
+            super(options)
 
-          if @current_order and current_store and @current_order.store.blank?
-            @current_order.update_attribute(:store_id, current_store.id)
+            if @current_order and current_store and @current_order.store.blank?
+              @current_order.update_attribute(:store_id, current_store.id)
+            end
+
+            @current_order
           end
-
-          @current_order
         end
-        alias_method_chain :current_order, :multi_domain
-      end
+      )
     end
 
     initializer 'spree.promo.register.promotions.rules' do |app|
